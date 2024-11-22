aa8f1f11e3f59bacff13640722c962d3bd84abac
caothienbao <baocaothien123@gmail.com>
2024-11-22 10:46:52 +0700

    Deploy 5

--- config-overrides.js
+++ config-overrides.js
@@ -1,19 +1,19 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 
 module.exports = function override(config) {
   config.resolve.fallback = {
     ...config.resolve.fallback,
-    https: require.resolve("https-browserify"),
-    http: require.resolve("stream-http"),
-    url: require.resolve("url"),
-    buffer: require.resolve("buffer"),
-    process: require.resolve("process/browser.js"), // Ensure the extension is specified
+    https: require.resolve('https-browserify'),
+    http: require.resolve('stream-http'),
+    buffer: require.resolve('buffer'),
+    url: require.resolve('url'),
+    process: require.resolve('process/browser'),
   };
 
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"],
-      process: "process/browser.js", // Use the explicit path with extension
+      process: 'process/browser',
+      Buffer: ['buffer', 'Buffer'],
     }),
   ]);
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/authContext'; // Import AuthProvider
+import './polyfills';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
