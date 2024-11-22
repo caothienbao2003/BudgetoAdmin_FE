4090a5d866950f957ee11762a913854473ff34f1
caothienbao <baocaothien123@gmail.com>
2024-11-22 10:04:05 +0700

    Change config for deploy

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,3 @@
-// config-overrides.js
 const webpack = require("webpack");
 
 module.exports = function override(config) {
@@ -7,13 +6,14 @@ module.exports = function override(config) {
     https: require.resolve("https-browserify"),
     http: require.resolve("stream-http"),
     url: require.resolve("url"),
-    buffer: require.resolve("buffer"), // Add buffer fallback
+    buffer: require.resolve("buffer"),
+    process: require.resolve("process/browser.js"), // Ensure the extension is specified
   };
 
-  // Optional: Add plugins to define global variables, like Buffer
   config.plugins = (config.plugins || []).concat([
     new webpack.ProvidePlugin({
       Buffer: ["buffer", "Buffer"],
+      process: "process/browser.js", // Use the explicit path with extension
     }),
   ]);
 
